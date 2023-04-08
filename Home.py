ffad7503817df16bdfd62068dd08b942e22a96f5
Chanakan1210 <130207077+Chanakan1210@users.noreply.github.com>
2023-04-08 14:24:24 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -63,11 +63,11 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0] == 'Setosa':
-    st.image("./pic/setosa.jpg")
+    st.image("./pic/Setosa.jpg")
    else:       
     st.writ('xxx')    
    #st.button("ไม่แสดงข้อมูล")
 else:
    st.write("ไม่แสดงข้อมูล")
-   
+
 #st.image("./pic/chanakan.jpg")
